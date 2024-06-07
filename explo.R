cc3a3c258b866cecfb902cb5ea14bee3a6cd3f0a
RaniaElFahli <rania.epro@gmail.com>
2024-06-07 11:07:37 +0200

    WIP

---
+++ explo.R
@@ -0,0 +1,97 @@
+library(dplyr) ; library(tidyverse)
+library(sf); library(ggplot2)
+
+
+# charger les shapefiles & mettre projection Lambert 93
+epci_fonds <- st_read("C:/Users/Rania El Fahli/Downloads/ADMIN-EXPRESS-COG_3-1__SHP_WGS84G_FRA_2022-04-15/ADMIN-EXPRESS-COG_3-1__SHP_WGS84G_FRA_2022-04-15/ADMIN-EXPRESS-COG/1_DONNEES_LIVRAISON_2022-04-15/ADECOG_3-1_SHP_WGS84G_FRA/EPCI.shp")
+
+mayotte <- st_read("C://Users//Rania El Fahli//Documents//mayotte.shp")
+epci_fonds <- sf::st_transform(epci_fonds, crs = 2154)
+mayotte <- sf::st_transform(mayotte, crs = 2154)
+
+# récupérer codes géo 22 des EPCI de chaque DROM
+source("C:/Users/Rania El Fahli/Documents/Atlas/Cartes/Scripts/codes_epci_dom.R")
+
+# séparer les fonds de cartes
+fonds_separes <- function(table_sf, code_geo, code_filtre) {
+  
+  france_m <- table_sf %>%
+    dplyr::filter(!{{code_geo}} %in% get(paste0(code_filtre, "_dom")))
+  print(paste("Exclure:", paste0(code_filtre, "_dom")))
+  print(head(france_m))
+  
+  guadeloupe <- table_sf %>%
+    dplyr::filter({{code_geo}} %in% get(paste0(code_filtre, "_guad")))
+  print(paste("Inclure Guadeloupe:", paste0(code_filtre, "_guad")))
+  print(head(guadeloupe))
+  
+  reunion <- table_sf %>%
+    dplyr::filter({{code_geo}} %in% get(paste0(code_filtre, "_reun")))
+  print(paste("Inclure Reunion:", paste0(code_filtre, "_reun")))
+  print(head(reunion))
+  
+  martinique <- table_sf %>%
+    dplyr::filter({{code_geo}} %in% get(paste0(code_filtre, "_mart")))
+  print(paste("Inclure Martinique:", paste0(code_filtre, "_mart")))
+  print(head(martinique))
+  
+  guyane <- table_sf %>%
+    dplyr::filter({{code_geo}} %in% get(paste0(code_filtre, "_guy")))
+  print(paste("Inclure Guyane:", paste0(code_filtre, "_guy")))
+  print(head(guyane))
+  
+  return(list(france_m = france_m, guadeloupe = guadeloupe, reunion = reunion, martinique = martinique, guyane = guyane))
+}
+
+fonds_separes_epci <- fonds_separes(
+  table_sf = epci_fonds, 
+  code_geo = CODE_SIREN, 
+  code_filtre = "epci"
+)
+
+# calcul des bbox : 
+
+bbox_drom_fm <- lapply(fonds_separes_epci, function(x) {
+  assign(paste0(names(x)), x |> sf::st_bbox(), envir = .GlobalEnv)
+})
+
+# Calcul des paramètres dx et dy de translation :
+dx_guadeloupe <- bbox_drom_fm[["france_m"]]$xmax + 100000 -  bbox_drom_fm[["guadeloupe"]]$xmin 
+dy_guadeloupe <- bbox_drom_fm[["france_m"]]$ymin - 100000 - bbox_drom_fm[["guadeloupe"]]$ymin
+
+
+g <- fonds_separes_epci[["guadeloupe"]]
+test = st_geometry(g) + c(dx_guadeloupe , dy_guadeloupe)
+test_2 = sf::st_set_geometry(g, test)
+test_2 <- st_set_crs(test_2, 2154)
+mapview::mapview(test_2)
+
+
+# sans corse  -------------------------------------------------------------
+
+tab_22 <- COGugaison::table_supracom_2022
+
+epci_corse <- tab_22 |>
+  dplyr::filter(REG == "94") |>
+  select(c(EPCI)) |>
+  distinct(EPCI)
+epci_corse <- epci_corse$EPCI
+
+epci_fm_sc <- fonds_separes_epci[["france_m"]] |>
+  dplyr::filter(!CODE_SIREN %in% epci_corse)
+
+mapview::mapview(epci_fm_sc)
+# le xmax et ymin de france_m est par rapport à la corse, on essaye sans la CORSE 
+
+bbox_fm_sc <- sf::st_bbox(epci_fm_sc)
+
+dx_guadeloupe <- (bbox_fm_sc$xmin-  bbox_drom_fm[["guadeloupe"]]$xmin) - 95000
+dy_guadeloupe <- (bbox_fm_sc$ymax- bbox_drom_fm[["guadeloupe"]]$ymax) - 500000
+g <- fonds_separes_epci[["guadeloupe"]]
+test = st_geometry(g) + c(dx_guadeloupe , dy_guadeloupe)
+test_2 = sf::st_set_geometry(g, test)
+test_2 <- st_set_crs(test_2, 2154)
+mapview::mapview(test_2)
+
+test_fm_guad <- rbind(epci_fm_sc, test_2)
+plot(test_fm_guad)
